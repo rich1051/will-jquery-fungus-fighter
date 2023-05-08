c3965bbd922069101ce06f9ad3061950b8263d3d
Will Richardson <rich1051@umn.edu>
2023-05-08 17:06:35 -0500

    began function to link attack buttons to attack type

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ $(document).ready(onReady);
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusHP = 100;
+let playerAP = 100
 
 function onReady() {
-    
+    $('.attacks').on('click', functionAttack)
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
@@ -15,4 +17,20 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+
+function functionAttack() {
+    if ($('.attack-btn arcane-scepter')) {
+        console.log('arcane scepter activated!');
+    }
+    else if ($('.attack-btn entangle')) {
+        console.log('entangle activated!');
+    }
+    else if ($('.attack-btn dragon-blade')) {
+        console.log('dragon blade activated!');
+    }
+    else if ($('.attack-btn star-fire')) {
+        console.log('star fire activated!');
+    }
+
 }
