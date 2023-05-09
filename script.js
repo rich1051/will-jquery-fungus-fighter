f0a6dbf7ef2b32cd206ad681e19fc8548e4c6097
Will Richardson <rich1051@umn.edu>
2023-05-08 21:05:17 -0500

    created conditionals within attack functions reflecting AP and HP costs

--- script.js
+++ script.js
@@ -25,21 +25,89 @@ function onReady() {
 
 function attackArcane(event) {
     event.preventDefault()
+    if (playerAP < 12) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('jump');
+        $('.hp-text').text(fungusHP);
+    }
+    else if (fungusHP < 14) {
+        fungusHP = 0;
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('dead');
+        $('.hp-text').text(playerAP);
+    }
+    else {
+        playerAP -= 12;
+        fungusHP -= 14;
+        $('.hp-text').text(fungusHP);
+        $('.ap-text').text(playerAP);
+    }
     console.log('Arcane Scepter activated!');
     }
 
 function attackEntangle(event) {
     event.preventDefault()
+    if (playerAP < 23) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('jump');
+        $('.hp-text').text(fungusHP);
+    }
+    else if (fungusHP <= 9) {
+        fungusHP = 0;
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('dead');
+        $('.hp-text').text(playerAP);
+    }
+    else {
+        playerAP -= 23;
+        fungusHP -= 9;
+        $('.hp-text').text(fungusHP);
+        $('.ap-text').text(playerAP);
+    }
     console.log('Entangle activated!');
     }
 
 function attackDragon(event) {
     event.preventDefault()
+    if (playerAP < 38) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('jump');
+        $('.hp-text').text(fungusHP);
+    }
+    else if (fungusHP <= 47) {
+        fungusHP = 0;
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('dead');
+        $('.hp-text').text(playerAP);
+    }
+    else {
+        playerAP -= 38;
+        fungusHP -= 47;
+        $('.hp-text').text(fungusHP);
+        $('.ap-text').text(playerAP);
+    }
     console.log('Dragon Blade activated!');
     }
 
 function attackStar(event) {
     event.preventDefault()
+    if (playerAP < 33) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('jump');
+        $('.hp-text').text(fungusHP);
+    }
+    else if (fungusHP <= 25) {
+        fungusHP = 0;
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('dead');
+        $('.hp-text').text(playerAP);
+    }
+    else {
+        playerAP -= 33;
+        fungusHP -= 25;
+        $('.hp-text').text(fungusHP);
+        $('.ap-text').text(playerAP);
+    }
     console.log('Star Fire activated!');
     }
 
